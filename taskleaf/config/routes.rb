8cae3d9363d35134000f1e5bf92f56dd16fed264
ms919 <sakuraimiki919@gmail.com>
2021-07-02 18:18:21 +0900

    add trial_route

--- taskleaf/config/routes.rb
+++ taskleaf/config/routes.rb
@@ -7,5 +7,24 @@ Rails.application.routes.draw do
   end
   root "tasks#index"
   resources :tasks
+
+  # CRUD + other actions
+  # resources :tunes do
+  #   # play_tune => /tunes/:id/play => tunes#play
+  #   get 'play', on: :member
+  #   # find_tunes => /tunes/find => tunes#find
+  #   get 'find', on: :collection
+  #   # preview_new_tune => /tunes/new/preview => tunes#preview
+  #   get 'preview', on: :new
+  # end
+
+  # 構造化
+  # scope :hoge do
+  #   resources :users
+  # end
+  # scope path: 's' do
+  #   resources :users
+  # end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
